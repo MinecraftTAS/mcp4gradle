5df24596fdfa33bd9fc8831d1370ea87c83d50e9
PancakeTAS <pancake@mgnet.work>
2023-04-29 16:23:12 +0200

    Turn decompilation components into their own class

---
+++ src/main/java/com/minecrafttas/mcp4gradle/tools/ApplyDiff.java
@@ -0,0 +1,48 @@
+package com.minecrafttas.mcp4gradle.tools;
+
+import java.io.File;
+import java.lang.ProcessBuilder.Redirect;
+
+import com.minecrafttas.mcp4gradle.Utils;
+
+/**
+ * Apply diff wrapper class
+ */
+public class ApplyDiff {
+
+	private static final String APPLYDIFF_EXE = "https://data.mgnet.work/mcp4gradle/tools/applydiff.exe";
+	
+	private File dir;
+	private ProcessBuilder p;
+	
+	/**
+	 * Initializes Apply Diff
+	 * @param dir Directory
+	 */
+	public ApplyDiff(File dir) {
+		this.dir = dir;
+	}
+	
+	/**
+	 * Initialize the patcher
+	 * @param diff Diff file
+	 * @throws Exception Filesystem Exception
+	 */
+	public void init(File diff) throws Exception {
+		var applydiff = Utils.obtainTempFile(APPLYDIFF_EXE);
+		
+		this.p = new ProcessBuilder(applydiff.getAbsolutePath(), "--binary", "-p1", "-u", "-i", diff.getAbsolutePath(), "-d", "src/minecraft");
+		this.p.directory(this.dir);
+		this.p.redirectOutput(Redirect.DISCARD);
+		this.p.redirectError(Redirect.DISCARD);
+	}
+	
+	/**
+	 * Runs the patcher
+	 * @throws Exception
+	 */
+	public void run() throws Exception {
+		this.p.start().waitFor();
+	}
+
+}
