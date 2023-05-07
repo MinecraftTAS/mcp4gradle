4ce8108a08176acc3b6d8f0ecf2ac800ef48c667
PancakeTAS <pancake@mgnet.work>
2023-05-07 20:56:52 +0200

    Remove unused code

--- src/main/java/com/minecrafttas/mcp4gradle/Utils.java
+++ src/main/java/com/minecrafttas/mcp4gradle/Utils.java
@@ -1,14 +1,9 @@
 package com.minecrafttas.mcp4gradle;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
 
 import com.minecrafttas.mcp4gradle.tools.ApplyDiff;
 import com.minecrafttas.mcp4gradle.tools.Jad;
@@ -75,29 +70,6 @@ public class Utils {
         
         System.out.println("Successfully decompiled minecraft");
 	}
-
-	/**
-	 * Recursively copy a folder
-	 * @param source Source folder
-	 * @param target Desintation folder
-	 * @throws Exception Filesystem Exception
-	 */
-	public static void copyFolder(Path source, Path target) throws Exception {
-        Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
-
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                Files.createDirectories(target.resolve(source.relativize(dir)));
-                return FileVisitResult.CONTINUE;
-            }
-
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                Files.copy(file, target.resolve(source.relativize(file)), StandardCopyOption.REPLACE_EXISTING);
-                return FileVisitResult.CONTINUE;
-            }
-        });
-    }
 	
 	/**
 	 * Obtains a new temporary file
